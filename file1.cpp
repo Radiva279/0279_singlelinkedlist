aaaade43f166a4cbb9b4b6594ddefc6c97b8d2c7
Radiva279 <radivagalih12@gmail.com>
2025-05-09 09:08:26 +0700

    memanggil prosedur dan fungsi dan membuat tampilan menu

--- file1.cpp
+++ file1.cpp
@@ -110,3 +110,88 @@ public:
     }
 };
 
+int main()
+{
+    SingleLinkedList mhs;
+    int nim;
+    char ch;
+
+    do
+    {
+        cout << "Menu" << endl;
+        cout << "1. Menambah data kedalam list" << endl;
+        cout << "2. Menghapus data dari dalam list" << endl;
+        cout << "3. Menampilkan semua data didalam list" << endl;
+        cout << "4. Mencari data didalam list" << endl;
+        cout << "5. Keluar" << endl;
+        cout << endl
+            << "Masukkan pilihan (1-5): ";
+        cin >> ch;
+        switch (ch)
+        {
+            case '1':
+            {
+                mhs.addNode();
+                break;
+            }
+
+            case '2':
+            {
+                if (mhs.ListEmpty())
+                {
+                    cout << endl
+                        << "List Kosong" << endl;
+                    break;
+                }
+                cout << endl
+                    << "\nMasukkan no mahasiswa yang akan dihapus :";
+                cin >> nim;
+                if (mhs.delNode(nim) == false)
+                cout << endl
+                << "Data tidak ditemukan" << endl;
+                else
+                cout << endl
+                << "Data dengan nomor mahasiswa " << nim << " berhasil dihapus" << endl;
+            };            break;
+            case '3':
+            {
+                mhs.traverse();
+            }
+            break;
+
+            case '4':
+            {
+                if (mhs.ListEmpty() == true)
+                {
+                    cout << "\nList Kosong\n";
+                    break;
+
+                }
+                Node *previous, *current;
+                cout << endl
+                << "Masukkan no mahasiswa yang dicari :";
+                cin >> nim;
+                if (mhs.Search(nim, &previous, &current) == false)
+                cout << endl
+                << "Data tidak ditemukan" << endl;
+                else
+                {
+                    cout << endl
+                    << "Data ditemukan" << endl;
+                    cout << "\nNo Mahasiwa: " << current->noMhs << endl;
+                    cout << "\n";
+                }
+            }
+            break;
+            case '5':
+            {
+            }
+            break;
+            default:
+            {
+                cout << "Pilihan salah!." << endl;
+            }
+            break;
+        }
+    } while (ch != '5');    
+}
